0bab0c61c25f7ec08dfc354c830772fcb7671d2b
TheoLeyenda <theoleyenda1997@gmail.com>
2018-12-20 14:44:04 -0300

    Comentando funcion de colicion.

--- DashWar/Assets/Scripts/Collisions/AvatarGroundCollision.cs
+++ DashWar/Assets/Scripts/Collisions/AvatarGroundCollision.cs
@@ -2,11 +2,20 @@
 
 public class AvatarGroundCollision : GameCollisionAbstract
 {
-    public override string Collider1Tag { get { return Tags.PLAYER; } protected set { } }
-    public override string Collider2Tag { get { return Tags.GROUND; } protected set { } }
+    //PARA HACER OTRA CLASE DE COLICION COPIAR LA SINTACIS DE ESTA CLASE
 
+    //en esta parte defino los tags de los dos collisionadores
+
+    //defino el tag del primer colicionador
+    public override string Collider1Tag { get { return Tags.PLAYER; } protected set { } }
+    //defino el tag del segundo colicionador
+    public override string Collider2Tag { get { return Tags.GROUND; } protected set { } }
+    //-----------------------------------------------------------------------
+
+    //Hago la funcion de la colicion que resive los gameObject que van a colicionar y la colicion 2D
     public override void Resolve(GameObject collider1, GameObject collider2, Collision2D collision)
     {
+        //Aqui resuelvo la colicion especificando que pasa una vez colicionan los dos objetos.
         AvatarController ac = collider1.GetComponent<AvatarController>();
 
         ac.IsJumping = false;
