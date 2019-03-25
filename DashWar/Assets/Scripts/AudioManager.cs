2ed84c5ac767f24a416fb2977408444d0f2d4207
Daniel-ECB <danielcorzo@outlook.com>
2019-03-25 01:07:30 -0300

    Ya hay música de fondo, solo que por ahora es una sola pista para todas las escenas, falta colocar una pista por nivel o grupo de niveles.

---
+++ DashWar/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+    #region Fields
+
+    public static AudioManager instance;
+    private AudioSource mainSource;
+
+    #endregion
+
+    #region Properties
+
+    /// <summary>
+    /// Gets and sets the current audioclip to play.
+    /// </summary>
+    public AudioClip CurrentClip
+    {
+        get
+        {
+            return mainSource.clip;
+        }
+        set
+        {
+            mainSource.Stop();
+            mainSource.clip = value;
+            mainSource.Play();
+        }
+    }
+
+    #endregion
+
+    #region Methods
+
+    /// <summary>
+    /// Use this for initialization.
+    /// </summary>
+    void Awake ()
+    {
+        // Singleton
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        mainSource = GetComponent<AudioSource>();
+	}
+
+    #endregion
+}
